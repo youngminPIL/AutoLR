6ee1e066b2588794d929ab9d1575c6995f2a70a0
youngmin Ro <youngmin4920@gmail.com>
2020-12-07 16:17:57 +0900

    Update and rename train_CUB.py to train.py

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ from torchvision import datasets,  transforms
 from util.weight_variation_AutoLR import *
 import time
 import os
-from model_AutoLR import ft_net, ft_net_4_1, ft_net_stride_4_1
-
+from model_AutoLR import ft_net, ft_net_4_1
+from test_embedded import Get_test_results_single
 from tensorboard_logger import configure, log_value
 import json
 import copy
@@ -23,12 +23,15 @@ import copy
 parser = argparse.ArgumentParser(description='Training')
 parser.add_argument('--gpu_ids', default='0', type=str, help='gpu_ids: e.g. 0  0,1,2  0,2')
 parser.add_argument('--name', default='ft_ResNet50', type=str, help='output model name')
-parser.add_argument('--data_dir', default='/home/zzd/Market/pytorch', type=str, help='training dir path')
-parser.add_argument('--batchsize', default=32, type=int, help='batchsize')
+parser.add_argument('--batchsize', default=40, type=int, help='batchsize')
+parser.add_argument('--dataset', default='CUB-200', type=str, help='dataset')
+parser.add_argument('--max_f', default=0.4, type=float, help='max_f')
+parser.add_argument('--min_f', default=2, type=float, help='min_f')
+
+
 opt = parser.parse_args()
 
-data_dir = opt.data_dir
-name = opt.name
+
 str_ids = opt.gpu_ids.split(',')
 gpu_ids = []
 for str_id in str_ids:
@@ -36,36 +39,35 @@ for str_id in str_ids:
     if gid >= 0:
         gpu_ids.append(gid)
 
-data_dir = '/home/ro/FG/CUB_200_2011/pytorch'
-
-print(data_dir)
+if opt.dataset == 'CUB-200':
+    data_dir = '/home/ro/FG/CUB_RT/pytorch'
+elif opt.dataset == 'Cars-196':
+    data_dir = '/home/ro/FG/STCAR_RT/pytorch'
 
 scale = 1000000
 gamma = 0.2
 cls_lr = 0.01
 thr_score = 0.94
 
-e_drop = 30
-e_end = 40
+e_drop = 40
+e_end = 50
 mlast = 3
 
 desired_weva_set = []
-min_factor = 3
+min_factor = 2
 max_factor = 0.4
 
 conv1_factor = 0.5
 strict = False
-stride_ver = True
 save_few_epoch = False
-dir_name = '/data/ymro/CVPR2020_reproduce/RT_AUTO_CUB4_1_G2/SGD_thr94_lr3_min3_max04_ep3040_conv05_std'
+dir_name = '/data/ymro/AAAI2021/reproduce'
 
 configure(dir_name)
 print(dir_name)
 if not os.path.exists(dir_name):
     os.mkdir(dir_name)
-gpu_ids[0] = 2
+# gpu_ids[0] = opt.gpu_ids
 print(gpu_ids[0])
-opt.batchsize = 40
 # set gpu ids
 if len(gpu_ids) > 0:
     torch.cuda.set_device(gpu_ids[0])
@@ -74,6 +76,7 @@ if len(gpu_ids) > 0:
 # Load Data
 # ---------
 #
+init_resize = (256,256)
 resize = (224, 224)
 transform_train_list = [
     # transforms.RandomResizedCrop(size=128, scale=(0.75,1.0), ratio=(0.75,1.3333), interpolation=3), #Image.BICUBIC)
@@ -84,28 +87,40 @@ transform_train_list = [
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ]
 
+transform_val_list = [
+    transforms.Resize(init_resize, interpolation=3),  # Image.BICUBIC
+    transforms.CenterCrop(resize),  # Image.BICUBIC
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+]
 
 
 print(transform_train_list)
 data_transforms = {
-    'train': transforms.Compose(transform_train_list)
+    'train': transforms.Compose(transform_train_list),
+    'test': transforms.Compose(transform_val_list),
 }
 
 
 image_datasets = {}
 image_datasets['train'] = datasets.ImageFolder(os.path.join(data_dir, 'train'),
                                                data_transforms['train'])
+image_datasets['test'] = datasets.ImageFolder(os.path.join(data_dir, 'test'),
+                                               data_transforms['test'])
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=opt.batchsize,
-                                              shuffle=True, num_workers=16)
-               for x in ['train']}
+dataloaders = {}
+dataloaders['train'] = torch.utils.data.DataLoader(image_datasets['train'], batch_size=opt.batchsize, shuffle=True, num_workers=8)
+
+dataloaders['test'] = torch.utils.data.DataLoader(image_datasets['test'], batch_size=opt.batchsize, shuffle=False, num_workers=8)
+
+
+
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train']}
 class_names = image_datasets['train'].classes
 
 use_gpu = torch.cuda.is_available()
 
 inputs, classes = next(iter(dataloaders['train']))
-
 
 
 
@@ -130,7 +145,7 @@ def train_1epoch(phase, modelB, optimizer, epoch, trial):
         optimizer.zero_grad()
 
         # forward
-        outputs = modelB(inputs)
+        f, outputs = modelB(inputs)
         _, preds = torch.max(outputs.data, 1)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -164,8 +179,8 @@ def adjustLR(optimizer, weva_table, lr_table, score, n_epoch):
     if len(weva_table) <= 1:
         # Here we make desired weight variation(weva)
         if n_epoch == 0:
-            max_weva = max(now_weva)*max_factor
-            min_weva = min(now_weva)*min_factor
+            max_weva = max(now_weva)*opt.max_f
+            min_weva = min(now_weva)*opt.min_f
             print('Bound condition of weigh variation are Max: {:.6f} Min: {:.6f}'.format(max_weva,min_weva))
             bias = min_weva
             interval = (max_weva - min_weva) / (len(now_weva) - 1)
@@ -335,14 +350,16 @@ def train_model(model_pre, thr_score, criterion, optimizer_pre, drop_timing, num
             print('trial{}, score:{}, {} Loss: {:.8f} Acc: {:.8f}'.format(trial, score,
                 phase, epoch_loss, epoch_acc))
 
-            weva_try2 = weva_try[1:-3]
-            weva_try2.append(weva_try[-1])
+            results = Get_test_results_single(image_datasets['test'], dataloaders['test'], model_try)
 
-            epoLfmt = ['{:.6f}']*len(weva_try2)
+            weva_try_print = weva_try[1:-3]
+            weva_try_print.append(weva_try[-1])
+
+            epoLfmt = ['{:.6f}']*(len(weva_try_print)-1)
             epoLfmt =' '.join(epoLfmt)
             values = []
-            for i in range(len(weva_try2)):
-                values.append(weva_try2[i])
+            for i in range(len(weva_try_print)-1):
+                values.append(weva_try_print[i])
             epoLfmt = '   WeVa :' + epoLfmt
             print(epoLfmt.format(*values))
 
@@ -356,14 +373,15 @@ def train_model(model_pre, thr_score, criterion, optimizer_pre, drop_timing, num
                 epoLfmt = 'desWeVa :' + epoLfmt
                 print(epoLfmt.format(*values))
 
-            epoLfmt = ['{:.6f}'] * len(print_lr)
+            epoLfmt = ['{:.6f}'] * (len(print_lr)-1)
             epoLfmt = ' '.join(epoLfmt)
             values = []
-            for i in range(len(print_lr)):
+            for i in range(len(print_lr)-1):
                 values.append(print_lr[i])
             epoLfmt = '     LR :' + epoLfmt
             print(epoLfmt.format(*values))
 
+            print('test accuracy : top-1 {:.4f} top-2 {:.4f} top-4 {:.4f} top-8 {:.4f}'.format(results[0]*100,results[1]*100,results[2]*100,results[3]*100))
 
             if phase == 'train':
                 log_value('train_loss', epoch_loss, epoch)
@@ -403,11 +421,8 @@ def load_network_path(network, save_path):
     network.load_state_dict(torch.load(save_path))
     return network
 
-if stride_ver == True:
-    model = ft_net_stride_4_1(len(class_names))
-    print('Stride 1 version network is successfully loaded!!')
-else:
-    model = ft_net_4_1(len(class_names))
+
+model = ft_net_4_1(len(class_names))
 
 
 if use_gpu:
@@ -445,8 +460,6 @@ optimizer_ft = optim.SGD([
 ], momentum=0.9, weight_decay=5e-4, nesterov=True) #for CUB
 
 
-#
-dir_name = os.path.join(dir_name, name)
 if not os.path.isdir(dir_name):
     os.mkdir(dir_name)
 
